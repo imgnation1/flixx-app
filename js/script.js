a6dbbf18b647428e64997f0139428d485da67a2b
kirill <kirill58010@yahoo.com>
2024-03-12 20:14:27 +0300

    brad traversy pretty eyes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const global = {
     type: "",
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: "b8e3dc172e488178475e290b653d105b",
@@ -246,14 +247,100 @@ async function search() {
   global.search.term = urlParams.get("search-term");
 
   if (global.search.term !== "" && global.search.term !== null) {
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.lenght === 0) {
       showAlert("No results found");
+      return;
     }
+
+    displaySearchResults(results);
+
+    document.querySelector("#search-term").value = "";
   } else {
     showAlert("Please enter the search term");
   }
+}
+
+function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector("#search-results").innerHTML = "";
+  document.querySelector("#search-results-heading").innerHTML = "";
+  document.querySelector("#pagination").innerHTML = "";
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<a href="${global.search.type}-details.html?id=${
+      result.id
+    }">
+		${
+      result.poster_path
+        ? `<img
+		src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+		class="card-img-top"
+		alt="${global.search.type === "movie" ? result.title : result.name}"
+	/>`
+        : `<img
+	src="../images/no-image.jpg"
+	class="card-img-top"
+	alt="${global.search.type === "movie" ? result.title : result.name}"
+/>`
+    }
+	</a>
+	<div class="card-body">
+		<h5 class="card-title">${
+      global.search.type === "movie" ? result.title : result.name
+    }</h5>
+		<p class="card-text">
+			<small class="text-muted">Release: ${
+        global.search.type === "movie"
+          ? result.release_date
+          : result.first_air_date
+      }</small>
+		</p>
+	</div>`;
+
+    document.querySelector("#search-results-heading").innerHTML = `
+			<h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+		`;
+
+    document.querySelector("#search-results").appendChild(div);
+  });
+
+  displayPagination();
+}
+
+// Create & Display Pagination for search
+function displayPagination() {
+  const div = document.createElement("div");
+  div.classList.add("pagination");
+  div.innerHTML = `
+	<button class="btn btn-primary" id="prev">Prev</button>
+	<button class="btn btn-primary" id="next">Next</button>
+	<div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+	`;
+
+  document.querySelector("#pagination").appendChild(div);
+
+  // Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector("#prev").disabled = true;
+  }
+  // disable next button
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector("#next").disabled = true;
+  }
+
+  // next page
+  document.querySelector("#next").addEventListener("click", async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
 }
 
 // Display slider movies
@@ -331,7 +418,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
